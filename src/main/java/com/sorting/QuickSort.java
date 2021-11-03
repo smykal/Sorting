5dcfc263b2376c9ac1c249441c2c6963069ff7b2
Mateusz <smykal@engineer.com>
2021-11-03 23:08:51 +0100

    edit QuickSort class

--- src/main/java/com/sorting/QuickSort.java
+++ src/main/java/com/sorting/QuickSort.java
@@ -3,7 +3,17 @@ package com.sorting;
 public class QuickSort implements Sort {
     @Override
     public int[] sortTable(int[] tableToSort) {
-        
+        quickSort(tableToSort,0, tableToSort.length-1);
         return new int[0];
+    }
+
+    private void quickSort(int[] tableToSort, int left, int right) {
+        if (left <right) {
+            int pivot = partition(tableToSort, left, right);
+        }
+    }
+
+    private int partition(int[] tableToSort, int left, int right) {
+        return 0;
     }
 }
